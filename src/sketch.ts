f7181667d0e39b4ed388899428a231168f8848e3
l00sed <danielwtompkins@gmail.com>
2022-03-05 13:23:17 -0500

    adding video recording feature by default

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,11 +1,56 @@
-import p5 from 'p5'
+import p5Typed from 'p5'
+import p5DOM from 'p5/lib/addons/p5.dom'
 
 /**
  * @param {p5} p
  */
-export const sketch = (p: p5) => {
+
+export const sketch = ( p: p5Typed, dom: p5DOM ) => {
+  const width: number = 150
+  const height: number = 150
+  const framerate: number = 30
+  let recording = false
+  let recorder
+  let chunks = []
+
+  const record = () => {
+    chunks.length = 0
+    let stream = document.querySelector( 'canvas' ).captureStream( framerate )
+    recorder = new MediaRecorder( stream )
+    recorder.ondataavailable = e => {
+      if ( e.data.size ) {
+        chunks.push( e.data )
+      }
+    }
+    recorder.onstop = exportVideo
+  }
+
+  const exportVideo = ( e ) => {
+    var blob = new Blob( chunks, { 'type' : 'video/webm' } )
+
+    // Draw video to screen
+    var videoElement = document.createElement( 'video ')
+    videoElement.setAttribute( "id", Date.now() )
+    videoElement.controls = true
+    document.body.appendChild( videoElement )
+    videoElement.src = window.URL.createObjectURL( blob )
+
+    // Download the video
+    var url = URL.createObjectURL( blob )
+    var a = document.createElement( 'a' )
+    document.body.appendChild( a )
+    a.style = 'display: none'
+    a.href = url
+    a.download = 'sketch.webm'
+    a.click()
+    window.URL.revokeObjectURL( url )
+  }
+
   p.setup = () => {
     // Define your initial environment props & other stuff here
+
+    // Leave this for video-recording functionality
+    record()
   }
 
   p.draw = () => {
@@ -13,9 +58,28 @@ export const sketch = (p: p5) => {
   }
 
   p.keyPressed = () => {
+    // toggle recording true or false
+    recording = !recording
+    console.log( "RECORDING: " )
+    console.log(recording)
+
+    // Export sketch's canvas to file when pressing "r"
+    // if recording now true, start recording
+    if ( p.keyCode === 82 && recording ) {
+      console.log( ".webm recording started" );
+      recorder.start();
+    }
+
+    // if we are recording, stop recording
+    if ( p.keyCode === 82 && !recording) {
+      console.log( ".webm recording stopped" );
+      recorder.stop();
+    }
+
     // Export sketch's canvas to file when pressing "p"
     if ( p.keyCode === 80 ) {
-      p.saveCanvas('sketch', 'png')
+      console.log( "saving .png" )
+      p.saveCanvas( 'sketch', 'png' )
     }
   }
 }
