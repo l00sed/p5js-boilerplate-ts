fa7e37615043c5dc2e287dd85595ba2b643e1376
l00sed <danielwtompkins@gmail.com>
2022-03-10 17:10:41 -0500

    added canvas setup

--- src/sketch.ts
+++ src/sketch.ts
@@ -6,82 +6,88 @@ import p5DOM from 'p5/lib/addons/p5.sound'
  * @param {p5} p
  */
 
-export const sketch = ( p: p5Typed ) => {
+export const sketch = (p:p5Typed) => {
   let recording = false
   let recorder
   let chunks = []
 
   const record = () => {
     chunks.length = 0
-    let stream = document.querySelector( 'canvas' ).captureStream( framerate )
-    recorder = new MediaRecorder( stream )
+    let stream = document.querySelector('canvas').captureStream(framerate)
+    recorder = new MediaRecorder(stream)
     recorder.ondataavailable = e => {
-      if ( e.data.size ) {
-        chunks.push( e.data )
+      if (e.data.size) {
+        chunks.push(e.data)
       }
     }
     recorder.onstop = exportVideo
   }
 
   const exportVideo = () => {
-    var blob = new Blob( chunks, { 'type' : 'video/mp4' } )
+    var blob = new Blob(chunks, {'type': 'video/mp4'})
 
     // Draw video to screen
-    var videoElement = document.createElement( 'video')
-    videoElement.setAttribute( 'id', Date.now().toString() )
+    var videoElement = document.createElement('video')
+    videoElement.setAttribute('id', Date.now().toString())
     videoElement.controls = true
-    document.body.appendChild( videoElement )
-    videoElement.src = window.URL.createObjectURL( blob )
+    document.body.appendChild(videoElement)
+    videoElement.src = window.URL.createObjectURL(blob)
 
     // Download the video
-    var url = URL.createObjectURL( blob )
-    var a = document.createElement( 'a' )
-    document.body.appendChild( a )
-    a.setAttribute( 'style', 'display: none' )
+    var url = URL.createObjectURL(blob)
+    var a = document.createElement('a')
+    document.body.appendChild(a)
+    a.setAttribute('style', 'display:none')
     a.href = url
     a.download = 'sketch.mp4'
     a.click()
-    window.URL.revokeObjectURL( url )
+    window.URL.revokeObjectURL(url)
   }
 
-  const width: number = 150
-  const height: number = 150
-  const framerate: number = 30
+  const width:number = 200
+  const height:number = 200
+  const framerate:number = 30
 
   p.setup = () => {
     // Define your initial environment props & other stuff here
+    p.createCanvas(width, height)
+    p.frameRate(framerate)
 
     // Leave this for video-recording functionality
     record()
   }
 
+  let t:number = 0 // loop counter
+
   p.draw = () => {
     // Define render logic for your sketch here
+
+    t+=1 // counter
   }
 
   p.keyPressed = () => {
     // toggle recording true or false
     recording = !recording
-    console.log( "RECORDING: " )
+    console.log('RECORDING: ')
     console.log(recording)
 
     // Export sketch's canvas to file when pressing "r"
     // if recording now true, start recording
-    if ( p.keyCode === 82 && recording ) {
-      console.log( ".mp4 recording started" )
+    if (p.keyCode === 82 && recording) {
+      console.log('.mp4 recording started')
       recorder.start()
     }
 
     // if we are recording, stop recording
-    if ( p.keyCode === 82 && !recording) {
-      console.log( ".mp4 recording stopped" )
+    if (p.keyCode === 82 && !recording) {
+      console.log('.mp4 recording stopped')
       recorder.stop()
     }
 
     // Export sketch's canvas to file when pressing "p"
-    if ( p.keyCode === 80 ) {
-      console.log( "saving .png" )
-      p.saveCanvas( 'sketch', 'png' )
+    if (p.keyCode === 80) {
+      console.log('saving .png')
+      p.saveCanvas('sketch', 'png')
     }
   }
 }
